34492f3e61481d0d879de13e9171d5d8604c89d2
danielmoi <daniel@ampersandmoi.com>
2017-06-22 06:33:57 +1000

    Get logic working

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const moment = require('moment');
 
 const app = express();
 const port = process.argv[2] || 5001;
@@ -12,8 +13,32 @@ app.set('view engine', 'jade');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const getUnix = data => moment(data).valueOf();
+
+const getNatural = data => moment(data).format('DD MMMM YYYY');
+
 app.get('/convert/:data', (req, res) => {
   const data = req.params.data;
+  let unix = null;
+  let natural = null;
+  console.log('data:', data);
+
+  if (!isNaN(Number(data))) {
+    console.log('we have a number!');
+    const valid = Number(data);
+    return res.json({
+      unix: getUnix(Number(valid)),
+      natural: getNatural(valid),
+    });
+  } else if (moment(data).isValid()) {
+    console.log('we have a valid date!');
+    return res.json({
+      unix: getUnix(data),
+      natural: getNatural(data),
+    });
+  } else {
+    return res.json({ unix, natural });
+  }
   res.json({ data });
 });
 
