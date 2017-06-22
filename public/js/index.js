b33e187a145ee9ce63f0cfd88f3d98153b90bfb5
danielmoi <daniel@ampersandmoi.com>
2017-06-22 20:08:46 +1000

    Setup webpack and precss!

---
+++ public/js/index.js
@@ -0,0 +1,2 @@
+require('../precss/style.css');
+
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const precss = require('precss');
+const autoprefixer = require('autoprefixer');
+const postcssImport = require('postcss-import');
+
+module.exports = {
+  context: path.resolve(__dirname, './'),
+  entry: {
+    app: './public/js/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './public/dist'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [
+                  postcssImport({
+                    addDependencyTo: webpack,
+                  }),
+                  precss(),
+                  autoprefixer(),
+                ],
+              },
+            },
+          ],
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+  ],
+};
